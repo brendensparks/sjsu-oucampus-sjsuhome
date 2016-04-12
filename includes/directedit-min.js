611c7fa5035df3fe18b1a6c982a22436f5b1a8df
Brenden Sparks <brenden.sparks@gmail.com>
2016-04-12 11:42:27 -0700

    Removed non-library .min files from repo

--- includes/directedit-min.js
+++
@@ -1,5 +0,0 @@
-function directedit() {
-	var link = document.getElementById("de").parentNode.innerHTML;
-	document.getElementById("de").parentNode.innerHTML = '';
-	document.getElementById("directedit").innerHTML = link;
-}
